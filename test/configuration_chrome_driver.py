719fe0a562746c3f518c5188d1888e22f19c4a05
Földi Péter <foldipeter.starjan@gmail.com>
2023-04-02 19:06:44 +0200

    Teszteléshez használt Selenium Chrome driver konigurcáiós modul bővítése távoli futtatáshoz.

--- test/configuration_chrome_driver.py
+++ test/configuration_chrome_driver.py
@@ -13,17 +13,21 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-def get_chrome_driver() -> Chrome:
+def get_chrome_driver(remote=True) -> Chrome:
     """Function that performs the configuration
 
     It currently makes Chrome webdriver twith speciel arguments.
+
+    Arguments:
+        remote (bool): If remote mode True run headless Chrome
 
     Returns:
         Chrome: configurated Selenium Chrome webdriver
     """
     options = Options()
     options.add_experimental_option('detach', True)
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    if remote:
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
     return Chrome(service=Service(executable_path=ChromeDriverManager().install()), options=options)
